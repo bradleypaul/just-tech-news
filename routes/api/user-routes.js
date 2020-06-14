1036bab0cd19ba76fa6c7644ee60c82274d296cf
Paul Bradley <bradleypauld@gmail.com>
2020-06-14 18:05:30 -0500

    add login routing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
@@ -83,6 +84,28 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+    
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!'});
+  });
 });
 
 module.exports = router;
