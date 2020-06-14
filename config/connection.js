a80adc52e6436abf89d008913a16749df2c14b0d
Paul Bradley <bradleypauld@gmail.com>
2020-06-14 16:49:34 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-
+require('dotenv').config();
 const sequalize = new Sequelize('just_tech_news_db', 'paul', 'beans', {
   host: 'localhost',
   dialect: 'mysql',
