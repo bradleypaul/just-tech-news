fde6bd26fc5f363086107162a9abf534369dcc52
Paul Bradley <bradleypauld@gmail.com>
2020-06-19 18:50:53 -0500

    mass update

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -86,26 +86,27 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-  User.findOne({
+  const user = await User.findOne({
     where: {
       email: req.body.email
     }
-  }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
-      return;
-    }
-    
-    const validPassword = dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
-    res.json({ user: dbUserData, message: 'You are now logged in!'});
   });
+
+  if (!user) {
+    res.status(400).json({ message: 'No user with that email address!' });
+    return;
+  }
+
+  const validPassword = await user.checkPassword(req.body.password);
+
+  if (!validPassword) {
+    res.status(400).json({ message: 'Incorrect password!' });
+    return;
+  }
+  res.json({ user: dbUserData, message: 'You are now logged in!' });
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(port, () => console.log('Now listening'));
