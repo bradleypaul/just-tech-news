ed7aa85b3b169bcc889f13da68502c577dcb6595
Paul Bradley <bradleypauld@gmail.com>
2020-06-20 23:12:56 -0500

    bulk update

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const routes = require('./controllers');
+const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,11 +24,13 @@ const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(controllers);
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(port, () => console.log('Now listening'));
 });
 
