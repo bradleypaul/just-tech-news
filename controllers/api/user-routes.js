84f67bf14623b1d95e5a48f8c2351ffdbddb2ecc
Paul Bradley <bradleypauld@gmail.com>
2020-06-23 21:24:46 -0500

    add login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,7 +52,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -117,8 +125,25 @@ router.post('/login', async (req, res) => {
     res.status(400).json({ message: 'Incorrect password!' });
     return;
   }
-  res.json({ user: dbUserData, message: 'You are now logged in!' });
+  req.session.save(() => {
+    // declare session variables
+    req.session.user_id = user.id;
+    req.session.username = user.username;
+    req.session.loggedIn = true;
+
+    res.json({ user: user, message: 'You are now logged in!' });
+  });
 
 });
+
+router.post('/logout', (req, res) => {
+  if(req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,12 +20,12 @@ const sess = {
 const app = express();
 const port = process.env.PORT || 3001;
 
-
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(controllers);
-app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
